b30c1535dc14e9dfa058d463cb64bf94d1fd54fd
raamizz <rameezebrahim@transpost.co>
2024-06-16 12:56:06 +0530

    Updt

--- src/components/landing/Vision.jsx
+++ src/components/landing/Vision.jsx
@@ -7,7 +7,7 @@ const Vision = () => {
         <div className="bg-blue-400 p-8 rounded-lg shadow-lg h-full">
           <h2 className="text-2xl font-bold mb-4 text-white">Our Vision</h2>
           <p className="text-white leading-relaxed">
-            We seek to work towards forming an organization which, as it grows,
+            We seek to work towards forming an organization which as it grows,
             also promotes inclusive growth and conservation of the environment
           </p>
         </div>
